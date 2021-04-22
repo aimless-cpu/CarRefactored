a1e19cb76c169ce835453566f2e873f473b81fb4
aimless-cpu <s.grabher@protonmail.com>
2021-04-22 17:56:09 +0200

    class Car added

---
+++ src/com/company/Car.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Car {
+    //instance
+    private String color;
+    private int velocityMax;
+    private double price;
+    private double priceDiscount; //price - producer.discount
+    private double consumption;
+    private double consumptionIncrease; //consumption + engine.increase
+
+    //Constructor
+    public Car(String color, int velocityMax, double price, double priceDiscount; double consumption, double consumptionIncrease) {
+        this.color = color;
+        this.velocityMax = velocityMax;
+        this.price = price;
+        this.priceDiscount = priceDiscount;
+        this.consumption = consumption;
+        this.consumptionIncrease = consumptionIncrease;
+        
+    }
+
+
+
+
+
+}
