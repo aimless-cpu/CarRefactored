e66f226b851b7a2afe6aa4f84e8b8302b98ed728
aimless-cpu <s.grabher@protonmail.com>
2021-04-22 19:20:02 +0200

    getPriceReduced implemented

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -7,20 +7,38 @@ public class Car {
     private double price;
     private double priceDiscount; //price - producer.discount
     private double consumption;
-    private double consumptionIncrease; //consumption + engine.increase
+    private double consumptionIncreased; //consumption * 1.098
+    //private double amountFuel;
+    private double amountDriven;
+    private Engine engine;
+    private Producer producer;
 
-    //Constructor
-    public Car(String color, int velocityMax, double price, double priceDiscount; double consumption, double consumptionIncrease) {
+    //constructor
+    public Car(String color, int velocityMax, double price, double priceDiscount, double consumption, double consumptionIncreased,
+               Engine engine) {
         this.color = color;
         this.velocityMax = velocityMax;
         this.price = price;
         this.priceDiscount = priceDiscount;
         this.consumption = consumption;
-        this.consumptionIncrease = consumptionIncrease;
-        
+        this.consumptionIncreased = consumptionIncreased;
+        //Engine
+        this.engine = engine;
+        //Producer
+        this.producer = producer;
     }
 
+    //method
 
+    public double getPriceDiscount(double price, double discount) {
+        this.priceDiscount = this.price - this.producer.discount;
+        return priceDiscount;
+    }
+
+    public double hasConsumptionIncreased() {
+        if (amountDriven>50000)
+
+    }
 
 
 
